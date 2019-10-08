79130f818e8f4023fd3eb107a5d4396b5bcf73df
nathan.latino <nathan.latino@he-arc.ch>
2019-10-08 14:44:02 +0200

    delete email user

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -13,7 +13,7 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::resource('swipe', 'SwipeController');
+Route::resource('swipes', 'SwipeController');
 
 //Api Auth
 Auth::guard('api')->user(); // instance of the logged user
