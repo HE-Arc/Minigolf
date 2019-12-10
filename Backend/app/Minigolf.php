69cdd7e57754c4801d69b46da68ed8eb7d5ba2f3
nathan.latino <nathan.latino@he-arc.ch>
2019-12-10 13:15:52 +0100

    long lat

--- Backend/app/Minigolf.php
+++ Backend/app/Minigolf.php
@@ -21,8 +21,8 @@ class Minigolf extends Model
     {
         $faker = \Faker\Factory::create();
         if(empty($this->id)) {
-            $this->long = "46.{$faker->numberBetween(953316,856612)}";
-            $this->lat = "6.{$faker->numberBetween(997679,991582)}";
+            $this->long = "6.{$faker->numberBetween(997679,991582)}";
+            $this->lat = "46.{$faker->numberBetween(953316,856612)}";
             $this->image = "https://picsum.photos/id/{$faker->numberBetween(100,1080)}/800/600";
         }
         return parent::save($options);
