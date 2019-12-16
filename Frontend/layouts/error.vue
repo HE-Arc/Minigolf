6955b5f4464e84dab056c27c0c28e1ee13517538
roscas <sol.rosca@gmail.com>
2019-12-16 22:39:07 +0100

    error page

---
+++ Frontend/layouts/error.vue
@@ -0,0 +1,27 @@
+<template>
+  <Page :title="title">
+    <template v-slot:body>
+      <nuxt-link class="ml-2" to="/">Go back home</nuxt-link>
+    </template>
+  </Page>
+</template>
+
+<script>
+import Page from "../components/Page";
+export default {
+  components: { Page },
+  props: ["error"],
+  data: () => ({
+  }),
+  computed: {
+    title() {
+      let message = "Unexpected error";
+      let error = this.error.statusCode;
+      if (error == 404) {
+        message = "It's just a 404";
+      }
+      return message;
+    },
+  },
+};
+</script>
