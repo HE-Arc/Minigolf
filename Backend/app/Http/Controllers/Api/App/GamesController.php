b92ab7dd8036a1f6a983eaa02e054bbeeadad555
nathan.latino <nathan.latino@he-arc.ch>
2020-01-19 02:08:58 +0100

    block creation player more than one time

--- Backend/app/Http/Controllers/Api/App/GamesController.php
+++ Backend/app/Http/Controllers/Api/App/GamesController.php
@@ -18,23 +18,23 @@ class GamesController extends Controller
 {
     public function show($id)
     {
-        return UserResource::collection(User::with('gamesplayed.players.scores.hole','gamesplayed.course.minigolf')
-                ->where('id', $id)
-                ->get())
-                ->jsonSerialize()[0][0];
+        return UserResource::collection(User::with('gamesplayed.players.scores.hole', 'gamesplayed.course.minigolf')
+            ->where('id', $id)
+            ->get())
+            ->jsonSerialize()[0][0];
     }
 
     public function gameToken($token)
     {
-        return GameResource::collection(Game::with('players.scores.hole','course.minigolf')
-                ->where('token', $token)
-                ->get())
-                ->jsonSerialize()[0];
+        return GameResource::collection(Game::with('players.scores.hole', 'course.minigolf')
+            ->where('token', $token)
+            ->get())
+            ->jsonSerialize()[0];
     }
 
     public function store(Request $request)
     {
-        $course =  Course::all(['id', 'name'])
+        $course = Course::all(['id', 'name'])
             ->where('id', $request->course_id)->first();
 
 
@@ -49,10 +49,9 @@ class GamesController extends Controller
             'game_id' => $game->id
         ]);
 
-        $holes = Hole::all(['id','course_id'])
+        $holes = Hole::all(['id', 'course_id'])
             ->where('course_id', $course->id)
             ->flatten();
-
 
 
         foreach ($holes as $hole) {
@@ -69,25 +68,30 @@ class GamesController extends Controller
     public function joinGame(Request $request)
     {
         $game = Game::all(['id', 'token', 'course_id'])
-                        ->where('token', $request->token)->first();
+            ->where('token', $request->token)->first();
 
 
-        $player = factory(Player::class)->create([
-            'user_id' => $request->user_id,
-            'game_id' => $game->id
-        ]);
+        $playerExist = Player::all(['id', 'game_id', 'user_id'])
+            ->where('game_id', $game->id)
+            ->where('user_id', $request->user_id)->first();
 
-        $holes = Hole::all(['id','course_id'])
-                    ->where('course_id', $game->course_id)->flatten();
-
-
-
-        foreach ($holes as $hole) {
-            factory(Score::class)->create([
-                'hole_id' => $hole->id,
-                'player_id' => $player->id,
-                'score' => 0,
+        if ($playerExist == null) {
+            $player = factory(Player::class)->create([
+                'user_id' => $request->user_id,
+                'game_id' => $game->id
             ]);
+
+            $holes = Hole::all(['id', 'course_id'])
+                ->where('course_id', $game->course_id)->flatten();
+
+
+            foreach ($holes as $hole) {
+                factory(Score::class)->create([
+                    'hole_id' => $hole->id,
+                    'player_id' => $player->id,
+                    'score' => 0,
+                ]);
+            }
         }
 
 
@@ -97,16 +101,16 @@ class GamesController extends Controller
     public function addScore($request)
     {
         $game = Game::all(['id', 'token'])
-                        ->where('token', $request->token);
+            ->where('token', $request->token);
 
         $player = factory(Player::class)->create([
             'user_id' => $request->user_id,
             'game_id' => $game->id
         ]);
 
-        $holes = Hole::all(['id','course_id'])
-                    ->where('course_id', $game->id)
-                    ->toArray();
+        $holes = Hole::all(['id', 'course_id'])
+            ->where('course_id', $game->id)
+            ->toArray();
 
         foreach ($holes as $hole) {
             factory(Score::class)->create([
